b208ae5b56df31aebd03036a63af9f02233e1b8a
MikaelReis <mikaelreis08@gmail.com>
2025-06-10 15:04:02 -0300

    feat: configurando Views

--- backend/profissionais/views.py
+++ backend/profissionais/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Profissional
+from .serializers import ProfissionalSerializer
+
+class ProfissionalViewSet(viewsets.ModelViewSet):
+    queryset = Profissional.objects.all()
+    serializer_class = ProfissionalSerializer
+
