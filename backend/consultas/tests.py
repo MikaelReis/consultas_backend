4d8b12f704c29442ea21a5c67a5ac14071f29f49
MikaelReis <mikaelreis08@gmail.com>
2025-06-10 17:27:21 -0300

    feat: realizando testes

--- backend/consultas/tests.py
+++ backend/consultas/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from backend.profissionais.models import Profissional
+from .models import Consulta
+
+class ConsultaTests(APITestCase):
+    def test_criar_consulta(self):
+        profissional = Profissional.objects.create(
+            nome="Dr. Marcos",
+            especialidade="Dermatologia",
+            email="marcos@exemplo.com"
+        )
+
+        data = {
+            "profissional": profissional.id,
+            "paciente": "Paciente 1",
+            "data": "2025-06-10",
+            "horario": "14:30:00"
+        }
+
+        response = self.client.post("/api/consultas/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Consulta.objects.count(), 1)
+        self.assertEqual(Consulta.objects.get().paciente, "Paciente 1")
+
+
+
